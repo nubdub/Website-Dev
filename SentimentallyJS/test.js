aa0b968ae84610637bb783aa1cf746ec603ba1fe
Nabdeep Shrestha <nabdeep@vt.edu>
2018-12-28 12:45:30 -0500

    Got working product

--- SentimentallyJS/test.js
+++ SentimentallyJS/test.js
@@ -1,33 +1,27 @@
-var express = require('express');
-var app = express();
+function y() {
+    console.log(x);
+    var x = 10;
+    console.log(x);
+    if (x > 5) {
+        var x = 2;
+        console.log('if: ' + x);
+    }
+    console.log('outside: ' + x);
+}
 
-app.use(express.static('public'));
 
-var car = {color: 'red"n"blue', door: 4, sunroof: true, price: 30000};
-var tweets = [
-    {value: "Hello", senti: 0.2},
-    {value: "Hello2", senti: 0.3},
-    {value: "Hello3", senti: 0.4},
-    {value: "Hello4", senti: 0.5},
-    {value: "Hello3", senti: 0.4},
-    {value: "Hello4", senti: 0.5},
-    {value: "Hello3", senti: 0.4},
-    {value: "Hello4", senti: 0.5},
-    {value: "Hello5", senti: 0.6}
-]
+function z() {
+    // console.log(x);
+    let x = 10;
+    console.log(x);
+    if (x > 5) {
+        let x = 2;
+        console.log('if: ' + x);
+    }
+    console.log('outside: ' + x);
+}
 
-app.get('/api', (req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    // res.send(JSON.stringify({data: 'Hello World'}));
-    res.send(car);
-});
+var x = 11;
 
-app.get('/api/tweets', (req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    // res.send(JSON.stringify({data: 'Hello World'}));
-    res.send(tweets);
-});
-
-console.log('Listening on port 8080');
-
-app.listen(8080);
+y();
+z();
